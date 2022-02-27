54df901c2c2662e492b674e407b2a86bfc270050
shahmilav <53659750+shahmilav@users.noreply.github.com>
2022-02-27 00:18:53 -0500

    ZSH aliases.

--- zsh/aliases/termcmds-alias.zsh
+++ zsh/aliases/termcmds-alias.zsh
@@ -2,8 +2,8 @@
 
 # ---< HELPFUL TERMINAL COMMANDS >---
 alias bel="tput bel"
-alias ll="echo; ls -lt"
-alias ls="dir --color=auto -GFh"
+alias ll="echo; dir -lFh --color=auto"
+alias ls="dir --color=auto -hF"
 alias py="python3.10"
 alias bat="bat ---theme Nord"
 alias sys="btm -b ---color nord"
