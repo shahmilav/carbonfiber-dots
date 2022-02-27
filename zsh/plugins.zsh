92fb4bc4e9452ed8d92b5e90950ea8bf4f4a24a3
shahmilav <53659750+shahmilav@users.noreply.github.com>
2022-02-27 10:29:51 -0500

    Added plugins

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -5,3 +5,6 @@ source /opt/homebrew/share/zsh-autosuggestions/zsh-autosuggestions.zsh
 ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=#808080"
 
 source /opt/homebrew/opt/zsh-fast-syntax-highlighting/share/zsh-fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh
+
+
+for f ($ZDOTDIR/plugins/**/*.zsh(N.)) . $f
