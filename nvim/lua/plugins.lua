ed66519bbf48c2ea772868ce97bfc6323a515f2f
shahmilav <53659750+shahmilav@users.noreply.github.com>
2022-03-06 12:02:02 -0500

    Updated plugins' :

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -13,78 +13,79 @@ return require('packer').startup(function()
   use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
   require('plugin-config.treesitter')
 
-  -------------------------------------------------------------------------------------
+  ---------------------------------------------------------------------------------
   ---< THEMES >---
   use 'shaunsingh/nord.nvim'
   use 'kaicataldo/material.vim'
   use "shahmilav/darkside"
   use 'olimorris/onedarkpro.nvim'
   use 'cocopon/iceberg.vim'
+  use 'chriskempson/base16-vim'
 
-  require('onedarkpro').load()
+require('onedarkpro').load()
   -------------------------------------------------------------------------------------
 
-  ---< NVIM-TREE >---
-  use {
-    'kyazdani42/nvim-tree.lua', 
-    requires = { 'kyazdani42/nvim-web-devicons' }
-  } 
-  require('plugin-config.nvim-tree')
+    ---< NVIM-TREE >---
+    use {
+      'kyazdani42/nvim-tree.lua', 
+      requires = { 'kyazdani42/nvim-web-devicons' }
+    } 
+    require('plugin-config.nvim-tree')
 
-  ---< STATUSBAR >---
-  use {
-    'nvim-lualine/lualine.nvim',
-    requires = { 'kyazdani42/nvim-web-devicons' }
-  }
-  require('lualine').setup()
+    ---< STATUSBAR >---
+    use {
+      'nvim-lualine/lualine.nvim',
+      requires = { 'kyazdani42/nvim-web-devicons' }
+    }
+    require('lualine').setup()
 
-  ---< TELESCOPE >---
-  use {
-    'nvim-telescope/telescope.nvim',
-    requires = { 'nvim-lua/plenary.nvim' }
-  }
-  require('plugin-config.tele')
+    ---< TELESCOPE >---
+    use {
+      'nvim-telescope/telescope.nvim',
+      requires = { 'nvim-lua/plenary.nvim' }
+    }
+    require('plugin-config.tele')
 
-  ---< DASHBOARD >---
-  use {
-    "startup-nvim/startup.nvim",
-    requires = {"nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim"},
-    config = function()
-      require"startup".setup()
-    end
-  }
-  require('plugin-config.dashboard-nvim')
+    ---< DASHBOARD >---
+    use {
+      "startup-nvim/startup.nvim",
+      requires = {"nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim"},
+      config = function()
+        require"startup".setup()
+      end
+    }
+    require('plugin-config.dashboard-nvim')
 
-  ---< INDENT BLANKLINE >---
-  use "lukas-reineke/indent-blankline.nvim"
-  require('plugin-config.indent-blankline')
+    ---< INDENT BLANKLINE >---
+    use "lukas-reineke/indent-blankline.nvim"
+    require('plugin-config.indent-blankline')
 
-  ---< COKELINE (TABS) >---
-  use {
-    'noib3/nvim-cokeline',
-    requires = 'kyazdani42/nvim-web-devicons'
-  } 
-  require('plugin-config.cokeline')
+    ---< COKELINE (TABS) >---
+    use {
+      'noib3/nvim-cokeline',
+      requires = 'kyazdani42/nvim-web-devicons'
+    } 
+    require('plugin-config.cokeline')
 
-  ---< GIT >---
-  use 'tpope/vim-fugitive'
-  use {
-    'lewis6991/gitsigns.nvim',
-    requires = {
-      'nvim-lua/plenary.nvim'
-    },
-    config = function()
-      require('gitsigns').setup()
-    end
-  }
+    ---< GIT >---
+    use 'tpope/vim-fugitive'
+    use {
+      'lewis6991/gitsigns.nvim',
+      requires = {
+        'nvim-lua/plenary.nvim'
+      },
+      config = function()
+        require('gitsigns').setup()
+      end
+    }
 
-  ---< COMPLETION >---
-  use {'neoclide/coc.nvim', branch = 'release'}
-  use 'rstacruz/vim-closer'
-  use 'tpope/vim-endwise'
+    ---< COMPLETION >---
+    use {'neoclide/coc.nvim', branch = 'release'}
+    use 'rstacruz/vim-closer'
+    use 'tpope/vim-endwise'
 
-  --< LSP >--
-  use 'neovim/nvim-lspconfig' -- Collection of configurations for the built-in LSP
-  use 'williamboman/nvim-lsp-installer'
+    --< LSP >--
+    use 'neovim/nvim-lspconfig' -- Collection of configurations for the built-in LSP
+    use 'williamboman/nvim-lsp-installer'
 
-end)
+  end)
