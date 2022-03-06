7c37ffb23e9139a5e7ee03f7186f82f2ee199207
shahmilav <53659750+shahmilav@users.noreply.github.com>
2022-03-06 12:00:57 -0500

    Playing with custom prompt

--- zsh/minimal-prompt.zsh
+++ zsh/minimal-prompt.zsh
@@ -50,7 +50,6 @@ git_info() {
   fi
 
   local -a GIT_INFO
-  GIT_INFO+=( "\033[38;5;15m±" )
   [ -n "$GIT_STATUS" ] && GIT_INFO+=( "$GIT_STATUS" )
   [[ ${#DIVERGENCES[@]} -ne 0 ]] && GIT_INFO+=( "${(j::)DIVERGENCES}" )
   [[ ${#FLAGS[@]} -ne 0 ]] && GIT_INFO+=( "${(j::)FLAGS}" )
