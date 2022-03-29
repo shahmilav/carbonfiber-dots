5b681b7a73e8c162107e7b4d5f65fa3d32c1b9c8
shahmilav <53659750+shahmilav@users.noreply.github.com>
2022-03-29 17:58:51 -0400

    stopped using nvchad

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,31 +1,25 @@
-local present, impatient = pcall(require, "impatient")
+-- [[ 
+--  _         _  _       _
+-- (_)       (_)| |     | |
+--  _  _ __   _ | |_    | | _   _   __ _
+-- | || '_ \ | || __|   | || | | | / _` |
+-- | || | | || || |_  _ | || |_| || (_| |
+-- |_||_| |_||_| \__|(_)|_| \__,_| \__,_|
 
-if present then
-   impatient.enable_profile()
-end
+-- ]]--
 
-local core_modules = {
-   "core.options",
-   "core.autocmds",
-   "core.mappings",
-}
+-- [[
+-- The init.lua file typically has all the configuration for neovim.
+-- However, in order to keep it manageable, it only sources three files:
+-- 
+-- * Plugins, 
+-- * Keybindings (mappings), 
+-- * and Defaults.
+-- 
+-- Seperate plugin configuration are in seperate files sourced in the plugins.lua file.
+-- ]]
 
-for _, module in ipairs(core_modules) do
-   local ok, err = pcall(require, module)
-   if not ok then
-      error("Error loading " .. module .. "\n\n" .. err)
-   end
-end
+require('plugins')
+require('mappings')
+require('defaults')
 
--- non plugin mappings
-require("core.mappings").misc()
-
--- check if custom init.lua file exists
-if vim.fn.filereadable(vim.fn.stdpath "config" .. "/lua/custom/init.lua") == 1 then
-   -- try to call custom init, if not successful, show error
-  local ok, err = pcall(require, "custom")
-   if not ok then
-      vim.notify("Error loading custom/init.lua\n\n" .. err)
-   end
-   return
-end
