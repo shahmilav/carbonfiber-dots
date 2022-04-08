bfa7f67ef49bebc389233388afd1b3ee1785338e
shahmilav <53659750+shahmilav@users.noreply.github.com>
2022-04-08 15:48:16 -0400

    playing with fish

---
+++ fish/prompt.fish
@@ -0,0 +1,12 @@
+set --global hydro_color_error $fish_color_error
+set --global hydro_symbol_prompt λ
+set --global hydro_symbol_git_dirty •
+set --global hydro_symbol_git_ahead ↑
+set --global hydro_symbol_git_behind ↓
+set --global hydro_multiline false
+
+set --global hydro_color_pwd cyan
+set --global hydro_color_git magenta
+set --global hydro_color_prompt green
+set --global hydro_color_duration yellow
+set --global hydro_color_error $fish_color_error
