a9089cad605799e9b0787d14cf07335e143d33ae
Milav Shah <53659750+shahmilav@users.noreply.github.com>
2022-07-25 15:37:56 -0400

    Add dark mode appearance toggle button to bar

---
+++ sketchybar/items/darkmode.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+
+if [[ $(defaults read -g AppleInterfaceStyle 2>/dev/null) == "Dark" ]]
+then
+    sketchybar -m --set appearance icon="􀆭"
+
+else
+    sketchybar -m --set appearance icon="􀆹"
+fi
+
