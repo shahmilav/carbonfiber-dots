a8067360c43fb96f7ab08bd0472dd5646ef971f9
Milav Shah <shahmilav@gmail.com>
2023-05-31 14:28:18 -0400

    FISH SHELL

--- fish/aliases.fish
+++ fish/aliases.fish
@@ -3,9 +3,14 @@ abbr ga "git add"
 abbr gs "git status"
 abbr gc "git commit -m"
 abbr gd "git diff"
+abbr gp "git push"
+abbr gpl "git pull"
 
 abbr .. "cd ../"
 abbr ... "cd ../../"
 abbr .... "cd ../../../"
 
-
+function ds -a dir
+  printf "Disk Space for $dir:\t"
+  du -sh $dir | head -n1 | awk '{print $1;}'
+end
--- fish/config.fish
+++ fish/config.fish
@@ -1,8 +1,29 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+  echo Hello Milav\n | lolcat
 end
 
+set -U fish_greeting ""
 
-fish_add_path /opt/homebrew/bin
+fish_add_path /opt/homebrew/bin 
+fish_add_path /Users/milav/bin
+fish_add_path ~/Library/Application\ Support/JetBrains/Toolbox/scripts/
+fish_add_path ~/.cargo/bin
 
 source ~/.config/fish/aliases.fish
+
+test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
+
+## HYDRO PROMPT CUSTOMIZATION ##
+
+function fish_right_prompt 
+end
+
+set --global hydro_symbol_prompt "❯"
+set --global hydro_symbol_git_dirty "*"
+
+set --global hydro_color_pwd blue
+set --global hydro_color_git green
+set --global hydro_color_error red
+set --global hydro_color_prompt magenta
+set --global hydro_color_duration yellow
+
